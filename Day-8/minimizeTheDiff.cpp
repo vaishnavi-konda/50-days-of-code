c90f42a3677bae728946eaa2c0c01ad6c58acc71
Vaishnavi Konda <74097479+vaishnavi-konda@users.noreply.github.com>
2020-11-14 22:43:41 +0530

    Update minimizeTheDiff.cpp

--- Day-8/minimizeTheDiff.cpp
+++ Day-8/minimizeTheDiff.cpp
@@ -13,7 +13,7 @@ Example 1:
 Example 2:
     Input: K = 3, N = 5, Arr[] = {3, 9, 12, 16, 20}
     Output: 11
-    Explanation: The array can be modified as {6, 12, 9, 13, 17}.
+    Explanation: The array can be modified as {6, 6, 9, 13, 17}.
     The difference between the largest and the smallest is 17-6 = 11.
 */
 
