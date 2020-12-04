5e756d58c1a769bf30f93daf2f08f2d36fd93105
Vaishnavi Konda <kvaishnavi026@gmail.com>
2020-12-04 16:06:56 +0530

    Added algorithm in Day-28 code (triplet of given sum)

--- Day-28/tripletOfGivenSum.cpp
+++ Day-28/tripletOfGivenSum.cpp
@@ -5,13 +5,17 @@ Given an array A[] of N numbers and another number 'sum'.
 Print the three elements in A[] whose sum is exactly x.
 
 Example 1:
-Input : array[] = {1, 4, 45, 6, 10, 8}, sum = 13
+Input : array[] = {1, 4, 45, 6, 10, 8}, x = 13
 Output : The Triplet is 1, 4, 8
 
 Example 2:
-Input : array[] = {1, 2, 4, 3, 6}, sum = 10
+Input : array[] = {1, 2, 4, 3, 6}, x = 10
 Output : The Triplet is 1, 3, 6
 
+Algorithm : 
+1. Sort the given array.
+2. Iterate through the array, for every element array[i], check if there exists a pair of sum (x - array[i]).
+3. If there exits such pair for an element, then print the element along with the pair found.
 */
 #include <iostream>
 #include <algorithm>
